6e77aa1e2aa4b60662ee9d872f76259a77bf6113
Olexander <98491805+Olexander1111@users.noreply.github.com>
2026-05-03 22:31:35 +0300

    Refactor PsychicJsonResponse and PsychicJsonHandler
    Signed-off-by: Olexander <98491805+Olexander1111@users.noreply.github.com>

--- src/PsychicJson.cpp
+++ src/PsychicJson.cpp
@@ -1,128 +1,135 @@
 #include "PsychicJson.h"
 
-#ifdef ARDUINOJSON_6_COMPATIBILITY
-PsychicJsonResponse::PsychicJsonResponse(PsychicResponse* response, bool isArray, size_t maxJsonBufferSize) : __response(response),
-                                                                                                              _jsonBuffer(maxJsonBufferSize)
+PsychicJsonResponse::PsychicJsonResponse(PsychicResponse* response, bool isArray)
+    : PsychicResponseDelegate(response)
 {
-  response->setContentType(JSON_MIMETYPE);
-  if (isArray)
-    _root = _jsonBuffer.createNestedArray();
-  else
-    _root = _jsonBuffer.createNestedObject();
-}
-#else
-PsychicJsonResponse::PsychicJsonResponse(PsychicResponse* response, bool isArray) : PsychicResponseDelegate(response)
-{
-  setContentType(JSON_MIMETYPE);
-  if (isArray)
-    _root = _jsonBuffer.add<JsonArray>();
-  else
-    _root = _jsonBuffer.add<JsonObject>();
-}
-#endif
-
-JsonVariant& PsychicJsonResponse::getRoot()
-{
-  return _root;
+    setContentType(JSON_MIMETYPE);
+    _jsonBuffer.s = isArray ? "[]" : "{}";
 }
 
 size_t PsychicJsonResponse::getLength()
 {
-  return measureJson(_root);
+    return _jsonBuffer.s.length();
 }
 
 esp_err_t PsychicJsonResponse::send()
 {
-  esp_err_t err = ESP_OK;
-  size_t length = getLength();
-  size_t buffer_size;
-  char* buffer;
+    esp_err_t    err         = ESP_OK;
+    const size_t length      = getLength();
+    const size_t buffer_size = (length < JSON_BUFFER_SIZE) ? (length + 1) : JSON_BUFFER_SIZE;
 
-  // how big of a buffer do we want?
-  if (length < JSON_BUFFER_SIZE)
-    buffer_size = length + 1;
-  else
-    buffer_size = JSON_BUFFER_SIZE;
+    char* buffer = static_cast<char*>(malloc(buffer_size));
+    if (!buffer)
+        return error(HTTPD_500_INTERNAL_SERVER_ERROR, "Unable to allocate memory.");
 
-  buffer = (char*)malloc(buffer_size);
-  if (buffer == NULL) {
-    return error(HTTPD_500_INTERNAL_SERVER_ERROR, "Unable to allocate memory.");
-  }
+    if (length < JSON_BUFFER_SIZE) {
+        memcpy(buffer, _jsonBuffer.s.c_str(), length);
+        buffer[length] = '\0';
+        setContent(reinterpret_cast<uint8_t*>(buffer), length);
+        setContentType(JSON_MIMETYPE);
+        err = PsychicResponseDelegate::send();
+    } else {
+        ChunkPrinter dest(_response, reinterpret_cast<uint8_t*>(buffer), buffer_size);
+        sendHeaders();
 
-  // send it in one shot or no?
-  if (length < JSON_BUFFER_SIZE) {
-    serializeJson(_root, buffer, buffer_size);
+        const char* data = _jsonBuffer.s.c_str();
+        size_t      sent = 0;
 
-    setContent((uint8_t*)buffer, length);
-    setContentType(JSON_MIMETYPE);
+        while (sent < length) {
+            const size_t chunk = std::min(buffer_size - 1, length - sent);
+            memcpy(buffer, data + sent, chunk);
+            buffer[chunk] = '\0';
 
-    err = PsychicResponseDelegate::send();
-  } else {
-    // helper class that acts as a stream to print chunked responses
-    ChunkPrinter dest(_response, (uint8_t*)buffer, buffer_size);
+            if (dest.write(reinterpret_cast<uint8_t*>(buffer), chunk) != chunk) {
+                err = ESP_FAIL;
+                break;
+            }
+            sent += chunk;
+        }
 
-    // keep our headers
-    sendHeaders();
+        dest.flush();
+        err = finishChunking();
+    }
 
-    serializeJson(_root, dest);
-
-    // send the last bits
-    dest.flush();
-
-    // done with our chunked response too
-    err = finishChunking();
-  }
-
-  // let the buffer go
-  free(buffer);
-
-  return err;
+    free(buffer);
+    return err;
 }
 
-#ifdef ARDUINOJSON_6_COMPATIBILITY
-PsychicJsonHandler::PsychicJsonHandler(size_t maxJsonBufferSize) : _onRequest(NULL),
-                                                                   _maxJsonBufferSize(maxJsonBufferSize) {};
+PsychicJsonHandler::PsychicJsonHandler(size_t maxContentLength)
+    : _onRequest(nullptr), _maxContentLength(maxContentLength),
+      _bodyBuffer(nullptr), _bodyBufferSize(0) {}
 
-PsychicJsonHandler::PsychicJsonHandler(PsychicJsonRequestCallback onRequest, size_t maxJsonBufferSize) : _onRequest(onRequest),
-                                                                                                         _maxJsonBufferSize(maxJsonBufferSize)
+PsychicJsonHandler::PsychicJsonHandler(PsychicJsonRequestCallback onRequest,
+                                       size_t maxContentLength)
+    : _onRequest(std::move(onRequest)), _maxContentLength(maxContentLength),
+      _bodyBuffer(nullptr), _bodyBufferSize(0) {}
+
+PsychicJsonHandler::~PsychicJsonHandler()
 {
+    cleanupBuffer();
 }
-#else
-PsychicJsonHandler::PsychicJsonHandler() : _onRequest(NULL) {};
 
-PsychicJsonHandler::PsychicJsonHandler(PsychicJsonRequestCallback onRequest) : _onRequest(onRequest)
+void PsychicJsonHandler::cleanupBuffer() noexcept
 {
+    if (_bodyBuffer) {
+        free(_bodyBuffer);
+        _bodyBuffer     = nullptr;
+        _bodyBufferSize = 0;
+    }
 }
-#endif
 
 void PsychicJsonHandler::onRequest(PsychicJsonRequestCallback fn)
 {
-  _onRequest = fn;
+    _onRequest = std::move(fn);
 }
 
 esp_err_t PsychicJsonHandler::handleRequest(PsychicRequest* request, PsychicResponse* response)
 {
-  // process basic stuff
-  PsychicWebHandler::handleRequest(request, response);
+    PsychicWebHandler::handleRequest(request, response);
 
-  if (_onRequest) {
-#ifdef ARDUINOJSON_6_COMPATIBILITY
-    DynamicJsonDocument jsonBuffer(this->_maxJsonBufferSize);
-    DeserializationError error = deserializeJson(jsonBuffer, request->body());
-    if (error)
-      return response->send(400);
+    if (!_onRequest)
+        return response->send(500, "text/plain", "No handler configured");
 
-    JsonVariant json = jsonBuffer.as<JsonVariant>();
-#else
-    JsonDocument jsonBuffer;
-    DeserializationError error = deserializeJson(jsonBuffer, request->body());
-    if (error)
-      return response->send(400);
+    const size_t contentLen = request->contentLength();
 
-    JsonVariant json = jsonBuffer.as<JsonVariant>();
-#endif
+    if (contentLen > _maxContentLength)
+        return response->send(413, "text/plain", "Content too large");
+    if (contentLen == 0)
+        return response->send(400, "text/plain", "Empty request body");
 
-    return _onRequest(request, response, json);
-  } else
-    return response->send(500);
+    const String contentType = request->contentType();
+    if (!contentType.equalsIgnoreCase(JSON_MIMETYPE))
+        return response->send(400, "text/plain", "Content-Type must be application/json");
+
+    _bodyBuffer = static_cast<uint8_t*>(malloc(contentLen + 1));
+    if (!_bodyBuffer)
+        return response->send(500, "text/plain", "Out of memory");
+
+    const String body = request->body();
+    int received = 0;
+
+    if (body.length() == contentLen) {
+        memcpy(_bodyBuffer, body.c_str(), contentLen);
+        received = contentLen;
+    } else {
+        httpd_req_t* req = request->request();
+        received = httpd_req_recv(req, reinterpret_cast<char*>(_bodyBuffer), contentLen);
+        if (received <= 0) {
+            cleanupBuffer();
+            return response->send(400, "text/plain", "Failed to read request body");
+        }
+    }
+
+    _bodyBuffer[received] = '\0';
+    _bodyBufferSize = received;
+
+    gson::Parser parser;
+    if (!parser.parse(reinterpret_cast<char*>(_bodyBuffer), _bodyBufferSize) || parser.hasError()) {
+        cleanupBuffer();
+        return response->send(400, "text/plain", "Invalid JSON");
+    }
+
+    esp_err_t result = _onRequest(request, response, parser);
+    cleanupBuffer();
+    return result;
 }
